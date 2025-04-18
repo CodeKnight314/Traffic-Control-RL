ed7f7ba7d4411a95382f6300a31cac530ddadff1
CodeKnight314 <tang.ri@northeastern.edu>
2025-04-18 01:27:52 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@ pip install -r requirements.txt
 add-apt-repository ppa:sumo/stable
 apt-get update
 apt-get install sumo sumo-tools sumo-doc
-echo 'export SUMO_HOME="/usr/share/sumo"' >> ~/.bashrc
-source ~/.bashrc
-apt-get install tmux
+export SUMO_HOME="/usr/share/sumo"
+apt-get install tmux xvfb

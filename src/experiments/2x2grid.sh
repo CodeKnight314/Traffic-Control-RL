9c8d9892d1ce3450ce93f53e1c598d53b6b0940f
CodeKnight314 <tang.ri@northeastern.edu>
2025-04-21 21:24:36 +0100

    Updating experiment .sh to use base_policy

--- src/experiments/2x2grid.sh
+++ src/experiments/2x2grid.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python main.py --config config.yaml --net routes/2x2grid/2x2.net.xml --route routes/2x2grid/2x2.rou.xml --path 2x2grid/weights/ --train
+python main.py --config config.yaml --net routes/2x2grid/2x2.net.xml --route routes/2x2grid/2x2.rou.xml --path 2x2grid/weights/ --train --weights ../resources/weights/base_policy_weights.pth
 xvfb-run --server-args="-screen 0 1024x768x24" python main.py --config config.yaml --net routes/2x2grid/2x2.net.xml --route routes/2x2grid/2x2.rou.xml --path 2x2grid/ --weight 2x2grid/weights/
--- src/experiments/4x4-Lucas-c1c2c1c2.sh
+++ src/experiments/4x4-Lucas-c1c2c1c2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python main.py --config config.yaml --net routes/4x4-Lucas/4x4.net.xml --route routes/4x4-Lucas/4x4c1c2c1c2.rou.xml --path 4x4_Lucas_c1c2c1c2/weights/ --train
+python main.py --config config.yaml --net routes/4x4-Lucas/4x4.net.xml --route routes/4x4-Lucas/4x4c1c2c1c2.rou.xml --path 4x4_Lucas_c1c2c1c2/weights/ --train --weights ../resources/weights/base_policy_weights.pth
 xvfb-run --server-args="-screen 0 1024x768x24" python main.py --config config.yaml --net routes/4x4-Lucas/4x4.net.xml --route routes/4x4-Lucas/4x4c1c2c1c2.rou.xml --path 4x4_Lucas_c1c2c1c2/ --weight 4x4_Lucas_c1c2c1c2/weights/

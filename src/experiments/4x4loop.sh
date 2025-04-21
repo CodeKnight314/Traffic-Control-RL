d112438de9aad24414cceba877b3132700c2b76b
CodeKnight314 <tang.ri@northeastern.edu>
2025-04-21 22:06:08 +0100

    Remove unnecessary 4x4 grid related bash code and resources

--- src/experiments/4x4loop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python main.py --config config.yaml --net routes/4x4loop/4x4loop.net.xml --route routes/4x4loop/4x4loop.rou.xml --path 4x4loop/weights/ --train --weights ../resources/weights/base_policy_weights.pth
-xvfb-run --server-args="-screen 0 1024x768x24" python main.py --config config.yaml --net routes/4x4loop/4x4loop.net.xml --route routes/4x4loop/4x4loop.rou.xml --path 4x4loop/ --weight 4x4loop/weights/
